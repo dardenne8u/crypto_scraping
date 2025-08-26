d8e1780e80b70f17665117eadcbd48e0634d16bf
luckonis <lucas.koenig@epitech.eu>
2025-08-26 15:29:07 +0200

    Modifiy: Change docker compose for add volumes

--- scraper/export_data.py
+++ scraper/export_data.py
@@ -4,6 +4,8 @@ import time
 producer = KafkaProducer(bootstrap_servers='kafka-1:19092')
 
 message = f"Message TEST 2"
+#for producer send it must to be created
+#Variable environment for docker
 producer.send('data_scraper', message.encode('utf-8'))
 print(f"Sent: {message}")
 time.sleep(2)
