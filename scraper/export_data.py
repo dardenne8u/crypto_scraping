d7305436f6a52b77469b918322d746f8de16d5e9
luckonis <lucas.koenig@epitech.eu>
2025-09-01 10:15:24 +0200

    Add topics part to export data

--- scraper/export_data.py
+++ scraper/export_data.py
@@ -1,14 +1,50 @@
-from kafka import KafkaProducer
+from kafka import KafkaProducer, KafkaAdminClient
+from kafka.errors import TopicAlreadyExistsError
 import time
 
-producer = KafkaProducer(bootstrap_servers='kafka-1:19092')
+def send_message_to_kafka(topic_name, message, bootstrap_servers):
+    try:
+        admin_client = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
+        topics = admin_client.list_topics()
+        # Check if the topic exists
+        if topic_name in topics:
+            print(f"Topic '{topic_name}' exists.")
+        else:
+            print(f"Topic '{topic_name}' does not exist.")
+            #Create topic if it does not exist
+            # raise Exception(f"Topic '{topic_name}' does not exist. Please create it manually.")
 
-message = f"Message TEST 2"
-#for producer send it must to be created
-#Variable environment for docker
-producer.send('data_scraper', message.encode('utf-8'))
-print(f"Sent: {message}")
-time.sleep(2)
+        # Initialize KafkaProducer
+        producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+        
+        # Send message to the specified topic
+        producer.send(topic_name, message.encode('utf-8'))
+        print(f"Sent: {message}")
+        
+        # Wait to ensure the message is sent before closing
+        time.sleep(2)
+        
+        # Ensure the producer sends all messages
+        producer.flush()
 
-producer.flush()
-producer.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Close the producer connection
+        producer.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scraper/main.py
+++ scraper/main.py
@@ -1,6 +1,7 @@
 import random
 import time
 from fetch_data import fetch_coinmarketcap_data
+from export_data import send_message_to_kafka
 
 #TODO: remove condition to break infinite loop and think about a better way to stop the script
 def main():
@@ -9,8 +10,12 @@ def main():
         waitNumber = random.randint(1, 5)
         print(waitNumber)
         time.sleep(waitNumber)
+        result = "Message TEST"
         # result = fetch_coinmarketcap_data()
         # print(result)
+
+        send_message_to_kafka('data_scraper', result,'kafka-1:19092')
+
 
         if i < 3:
             i += 1
