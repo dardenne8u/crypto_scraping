519f60dfa2813a36e4898f8199273f2ff754c91a
Corentin <corentin.labrux@epitech.eu>
2025-09-02 11:29:45 +0200

    maj : send for multiple brokers kafka

--- scraper/main.py
+++ scraper/main.py
@@ -1,8 +1,10 @@
 import random
 import time
+import os
 from fetch_data import fetch_coinmarketcap_data
 from export_data import send_message_to_kafka
 
+BrokerKafka = [os.getenv("KAFKA_BROKER_1"), os.getenv("KAFKA_BROKER_2"), os.getenv("KAFKA_BROKER_3")]
 
 def main():
     max_iterations = 3
@@ -18,7 +20,7 @@ def main():
         result = "Message TEST"
         print(f"Sending message: {result}")
 
-        send_message_to_kafka(topic_name='data_scraper', message=result, bootstrap_servers='kafka-1:19092')
+        send_message_to_kafka(topic_name='data_scraper', message=result, bootstrap_servers=BrokerKafka)
 
     print("Script ended after fixed iterations. Consider implementing a graceful shutdown.")
 
