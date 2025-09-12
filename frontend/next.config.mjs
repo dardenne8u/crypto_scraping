98cfd6fce9fa996ceff4470323454b8676ae8cd1
luckonis <lucas.koenig@epitech.eu>
2025-09-12 15:37:56 +0200

    Update gitignore

--- frontend/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['coin-images.coingecko.com', 'assets.coingecko.com'],
-  },
-};
-
-export default nextConfig;
