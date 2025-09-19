61cdbe1620e5aa41ddc94dddf5f94d7b625f23eb
Gr🥚 <gregory.dardenne@epitech.eu>
2025-09-19 15:56:20 +0200

    update db file to use environment variable

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,12 @@
-from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel import SQLModel, create_engine, Session, Annotated, Depends
+import os
 
-DATABASE_URL = "postgresql+psycopg2://tech:tech@localhost:9000/kafka"
+_username = os.getenv("DB_USER")
+_password = os.getenv("DB_PWD")
+_url = os.getenv("DB_URL")
+_db = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql+psycopg2://{_username}:{_password}@{_url}/{_db}"
 engine = create_engine(
     DATABASE_URL,
     echo=True,  # pour voir les requêtes SQL dans la console
@@ -12,6 +18,9 @@ def create_db():
 def get_session():
     with Session(engine) as session:
         yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
 
 class crypto(SQLModel, Table=True):
     name: str
