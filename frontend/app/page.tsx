485bf03043454d96a1ba68ba86227c18a15b4f02
luckonis <lucas.koenig@epitech.eu>
2025-11-13 15:24:32 +0100

    Fix page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,30 +1,56 @@
 "use client";
-
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
-import { DataTable } from "@/components/data-table";
+import { ChartAreaInteractive } from '@/components/chart-area-interactive'
+import { DataTable } from '@/components/data-table'
+import { SectionCards } from '@/components/section-cards'
+import { SiteHeader } from '@/components/site-header'
+import { fetchCryptoData } from '@/lib/crypto-api'
+import { io, Socket } from 'socket.io-client'
+import data from './data.json'
+import { SetStateAction, useEffect, useState } from 'react'
+import { CryptoData } from '@/types/crypto'
 import { SearchBar } from "@/components/ui/search";
-import { SiteHeader } from "@/components/site-header";
-import { useState } from "react";
-import data from "./data.json";
+
 
 export default function Page() {
+  const [messages, setMessages] = useState<CryptoData[]>([]);
   const [selectedName, setSelectedName] = useState<string | null>(null);
   const [search, setSearch] = useState<string>("");
-
   const handleSearch = (query: string) => {
     setSearch(query);
 
   };
-
   const filteredData = data.filter((item) =>
     item.name.toLowerCase().includes(search.toLowerCase())
   );
-
   const handleSelectName = (name: string) => {
     console.log("Select Name:", name);
     setSelectedName(name);
   };
 
+  useEffect(() => {
+    const socketInitializer = async () => {
+      const socket = new WebSocket('ws://localhost:9000/connect');
+      socket.onopen = () => {
+        console.log('connected')
+      }
+
+      socket.onmessage = (event) => {
+        try {
+          console.log("Received message:", event.data);
+          const data: CryptoData = JSON.parse(event.data);
+          setMessages([data]);
+        } catch (err) {
+          console.error("Error parsing message:", err);
+        }
+      };
+
+
+      socket.onclose = () => {
+        console.log('disconnected')
+      }
+    }
+    socketInitializer()
+  }, [])
   return (
     <div className="flex flex-col min-h-screen">
       <SiteHeader />
